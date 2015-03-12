522388581bcb3d8d29577dca465d7a78f25a285e
Shawn McKinney <smckinney@apache.org>
2015-03-12 12:03:51 -0500

    add secure bookmarkable links

--- src/main/java/org/wicketsample/MyBasePage.java
+++ src/main/java/org/wicketsample/MyBasePage.java
@@ -5,6 +5,7 @@ package org.wicketsample;
 
 import org.apache.directory.fortress.core.AccessMgr;
 import org.apache.directory.fortress.realm.J2eePolicyMgr;
+import org.apache.directory.fortress.web.FtBookmarkablePageLink;
 import org.apache.directory.fortress.web.SecUtils;
 import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.WebPage;
@@ -32,14 +33,16 @@ public abstract class MyBasePage extends WebPage
 
     public MyBasePage()
     {
-        //SecureBookmarkablePageLink page1Link = new SecureBookmarkablePageLink( "page1", Page1.class, "ROLE_TEST1" );
-        BookmarkablePageLink page1Link = new BookmarkablePageLink( "page1", Page1.class );
+        //BookmarkablePageLink page1Link = new BookmarkablePageLink( "page1.link", Page1.class );
+        FtBookmarkablePageLink page1Link = new FtBookmarkablePageLink( "page1.link", Page1.class );
         add( page1Link );
-        BookmarkablePageLink page2Link = new BookmarkablePageLink( "page2", Page2.class );
+        //BookmarkablePageLink page2Link = new BookmarkablePageLink( "page2.link", Page2.class );
+        FtBookmarkablePageLink page2Link = new FtBookmarkablePageLink( "page2.link", Page2.class );
         add( page2Link );
-        BookmarkablePageLink page3Link = new BookmarkablePageLink( "page3", Page3.class );
+        //BookmarkablePageLink page3Link = new BookmarkablePageLink( "page3.link", Page3.class );
+        FtBookmarkablePageLink page3Link = new FtBookmarkablePageLink( "page3.link", Page3.class );
         add( page3Link );
-        final Link actionLink = new Link( "logout" )
+        final Link actionLink = new Link( "logout.link" )
         {
             @Override
             public void onClick()
